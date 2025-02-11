004af9ac682e08688e4db3f14004ee897adc1f6d
Chan Badsha Bhuiyan <chanbadsha005@gmail.com>
2025-02-11 21:04:32 +0600

    Your commit message here

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,21 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-
 import { RouterProvider } from "react-router-dom";
 import router from "./Router/Routes.jsx";
 import AuthProvider from "./Provider/AuthProvider.jsx";
 import { HelmetProvider } from "react-helmet-async";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <HelmetProvider>
       <AuthProvider>
-        <RouterProvider router={router}></RouterProvider>
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router}></RouterProvider>
+        </QueryClientProvider>
       </AuthProvider>
     </HelmetProvider>
   </StrictMode>
